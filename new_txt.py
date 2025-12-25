87e5f73402fd48fd430c7360d95581e902f7f79c
Elena <elena.veritas11@gmail.com>
2025-12-25 15:08:09 +0300

    Update new_txt.py

--- new_txt.py
+++ new_txt.py
@@ -1,18 +1,27 @@
 import os
 
-source_txt = r"H:\tts\fin\outputs\full_kmeans\finnish_pseudo_for_vits.txt"
+source_txt = r"H:\tts\fin\filelists\finnish_pseudo_clean.txt"
 subset_txt = r"H:\tts\fin\subset_wav_list.txt"
 
 valid_lines = []
+total = 0
+found = 0
 
 with open(source_txt, "r", encoding="utf-8") as f:
     for line in f:
-        wav_path = line.strip().split("|")[0]  # путь до wav
+        total += 1
+        wav_path = line.strip().split("|")[0]
+
         if os.path.exists(wav_path):
             valid_lines.append(line)
+            found += 1
 
-# сохраняем новый .txt с реально существующими файлами
+        # 🔥 лог каждые 10k строк
+        if total % 10000 == 0:
+            print(f"Проверено: {total}, найдено wav: {found}")
+
 with open(subset_txt, "w", encoding="utf-8") as f:
     f.writelines(valid_lines)
 
-print(f"✅ Поднабор создан: {len(valid_lines)} строк")
+print(f"✅ ГОТОВО: {found} валидных строк из {total}")
+print(f"📄 Сохранено в: {subset_txt}")
